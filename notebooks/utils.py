c9b1aa12ca0a69c6e3a480bd94b5e49a48c31a23
Julien Rebetez <julien@fhtagn.net>
2016-06-05 22:24:49 +0200

    Some more experiments

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -3,6 +3,7 @@ import os
 import errno
 import glob
 import cv2
+import numpy as np
 
 def mkdir_p(path):
     try:
@@ -11,4 +12,14 @@ def mkdir_p(path):
         if exc.errno == errno.EEXIST:
             pass
         else: raise
-            
+
+def dice_coefficient(Y_pred, Y):
+    """
+    http://stackoverflow.com/a/31275008/116067
+    """
+    denom = (np.sum(Y_pred == 1) + np.sum(Y == 1))
+    if denom == 0:
+        # By definition, see https://www.kaggle.com/c/ultrasound-nerve-segmentation/details/evaluation
+        return 1
+    else:
+        return 2 * np.sum(Y[Y_pred == 1]) / denom
