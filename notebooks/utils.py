f6eb44447bedfefeaeb9712c4bfad701beaf5eec
Julien Rebetez <julien@fhtagn.net>
2016-06-05 11:20:17 +0200

    Initial commit - submission 3 @ 0.61

---
+++ notebooks/utils.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import errno
+import glob
+import cv2
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST:
+            pass
+        else: raise
+            
